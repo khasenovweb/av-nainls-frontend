560c1306fc8415937c86c59d1ce33e2b88afa692
khasenovweb <hasenovkirill93@gmail.com>
2024-02-18 19:54:43 +0500

    mac

--- js/main.js
+++ js/main.js
@@ -3,4 +3,9 @@ $(document).ready(function(){
         $(this).toggleClass('active');
         $('.hero__navbar__mobile').toggleClass('active');
     });
+
+
+    $('[data-parallax-scene]').each(function(i, el){
+        var parallaxInstance = new Parallax(el);
+    });
 });
