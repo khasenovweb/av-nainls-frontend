880a90956c203c3ae03a285454be044efb824d2b
khasenovweb <hasenovkirill93@gmail.com>
2024-02-23 09:30:26 +0500

    mac

--- js/main.js
+++ js/main.js
@@ -8,4 +8,23 @@ $(document).ready(function(){
     $('[data-parallax-scene]').each(function(i, el){
         var parallaxInstance = new Parallax(el);
     });
+
+
+    $('.materials__slider').owlCarousel({
+        items: 2,
+        margin: 26,
+        nav: true,
+        dots: true,
+        navText: ["<img src='/img/arrow-left.svg'>","<img src='/img/arrow-right.svg' >"],
+        dotsContainer: ".materials__slider__dots",
+        responsive: {
+            0: {
+                items: 1,
+            },
+            500: {
+                items: 2,
+            }
+        }
+    });
+
 });
